943155dd7d64ca6ac5fa1b3183a8aa274ddba4e7
Dom <djdomgo@gmail.com>
2023-07-05 08:30:58 +0800

    update css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,16 +14,16 @@ const Home = () => {
         </p>
         <Link href="/chat" className="p-2 bg-blue-500 text-white rounded">Start Chat</Link>
       </div>
-      <div className="mt-10 text-center">
-        <h2 className="text-xl mb-2">How to Use the AI Doctor Chatbot</h2>
-        <p className="mb-2">1. Click the &quot;Start Chat&quot; button to begin.</p>
-        <p className="mb-2">2. Describe your symptoms in as much detail as possible.</p>
-        <p className="mb-2">3. The chatbot will provide general health information based on your symptoms.</p>
-        <p className="mb-2">4. If necessary, consult a healthcare professional for further advice.</p>
+      <div className="mt-5 text-center">
+        <h2 className="text-xl mb-1">How to Use the AI Doctor Chatbot</h2>
+        <p className="mb-1">1. Click the &quot;Start Chat&quot; button to begin.</p>
+        <p className="mb-1">2. Describe your symptoms in as much detail as possible.</p>
+        <p className="mb-1">3. The chatbot will provide general health information based on your symptoms.</p>
+        <p className="mb-1">4. If necessary, consult a healthcare professional for further advice.</p>
       </div>
-      <div className="mt-10 text-center">
-        <h2 className="text-xl mb-2">Disclaimer</h2>
-        <p>This chatbot is not a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition.</p>
+      <div className="mt-2 text-center">
+        <h2 className="text-xs mb-2">Disclaimer</h2>
+        <p className="text-xs"> This chatbot is not a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified health provider with any questions you may have regarding a medical condition.</p>
       </div>
     </Page>
   );
