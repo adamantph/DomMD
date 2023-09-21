3080e6b0e74ec760375ddf73143bc248c394332d
TheWalkingBread <89442104+TheWalkingBread@users.noreply.github.com>
2023-09-22 05:30:55 +0800

    Fix user existence check

--- src/pages/api/getuser.js
+++ src/pages/api/getuser.js
@@ -3,6 +3,6 @@ import { sql } from '@vercel/postgres';
 export default async function handler(request, response) {
     const {email} = request.body;
     // console.log("Getting info for", username)
-    const user = await sql`SELECT * FROM users`;
+    const user = await sql`SELECT * FROM users where email = ${email}`;
     return response.status(200).json({user});
 }
